dfdee0282c57727d1c813735e329bd1bd69e5105
markgreenburg <marek.greenburg@gmail.com>
2017-04-20 23:40:39 -0500

    Added eslint configs & style updates

--- matchPairs.js
+++ matchPairs.js
@@ -1,65 +1,60 @@
 /**
- * Function ensures all pairs of braces, brackets, and parens are 
+ * Function ensures all pairs of braces, brackets, and parens are
  * opened and closed in a syntactically-correct order
  */
 
 const matchPairs = (charString) => {
-    
-    // First, do basic input validation and throw any obvious problems
-    if (!charString || typeof charString !== "string" || !charString.length) {
-        throw new Error("Must provide a non-empty input string")
+  // First, do basic input validation and throw any obvious problems
+  if (!charString || typeof charString !== 'string' || !charString.length) {
+    throw new Error('Must provide a non-empty input string');
+  }
+
+  // Define what chars can open and the corresponding close
+  const openers = ['{', '(', '['];
+  const closers = ['}', ')', ']'];
+
+  // Make input an array for easier iteration
+  const charArray = charString.split('');
+
+  // Make a stack to hold opened input chars
+  const charStack = [];
+
+  // Keep track of any unmatched closes
+  let unOpenedClose = false;
+
+  // Loop through input, making sure opens and closes happen in correct order
+  charArray.forEach((value) => {
+    // If input isn't one of allowed chars, throw an error
+    if (openers.indexOf(value) < 0 && closers.indexOf(value) < 0) {
+      throw new Error(`Found illegal '${value}' in input string`);
     }
 
-    // Define what chars can open and the corresponding close
-    const openers = [ "{", "(", "[" ];
-    const closers = [ "}", ")", "]" ];
+    // If character is an opener, add to stack and evaluate next index
+    if (openers.indexOf(value) >= 0) {
+      charStack.push(value);
+      return;
+    }
 
-    // Make input an array for easier iteration
-    const charArray = charString.split("");
+    // If character is a closer, check it and remove from stack
+    if (closers.indexOf(value) >= 0) {
+      // Make sure it's closing the right char by checking indexes of
+      // openers and closers
+      const lastStackItem = charStack[charStack.length - 1];
+      if (closers.indexOf(value) === openers.indexOf(lastStackItem)) {
+        // Great! Remove the last stack item and evaluate next index
+        charStack.pop();
+        return;
+      }
 
-    // Make a stack to hold opened input chars
-    let charStack = [];
+      // If current char isn't the right one to close the stack's open
+      // item, it must be an unmatched close and should fail the test
+      unOpenedClose = true;
+    }
+  });
 
-    // Keep track of any unmatched closes
-    let unOpenedClose = false;
-
-    // Loop through input, making sure opens and closes happen in correct order
-    charArray.forEach((value, index) => {
-
-        // If input isn't one of allowed chars, throw an error
-        if (openers.indexOf(value) < 0 && closers.indexOf(value) < 0) {
-            throw new Error("Found illegal '" + value + "' in input string");
-        }
-
-        // If character is an opener, add to stack and evaluate next index
-        if (openers.indexOf(value) >= 0) {
-            charStack.push(value);
-            return;
-        }
-
-        // If character is a closer, check it and remove from stack
-        if (closers.indexOf(value) >= 0) {
-
-            // Make sure it's closing the right char by checking indexes of
-            // openers and closers
-            const lastStackItem = charStack[charStack.length - 1];
-            if (closers.indexOf(value) === openers.indexOf(lastStackItem)) {
-                
-                // Great! Remove the last stack item and evaluate next index
-                charStack.pop();
-                return;
-            }
-
-            // If current char isn't the right one to close the stack's open
-            // item, it must be an unmatched close and should fail the test
-            unOpenedClose = true;
-            return;
-        }
-    });
-
-    // Our stack should be empty if everything was closed properly and we
-    // shouldn't have any unmatched closes. Test this and return the result
-    return (!charStack.length && !unOpenedClose);
-}
+  // Our stack should be empty if everything was closed properly and we
+  // shouldn't have any unmatched closes. Test this and return the result
+  return (!charStack.length && !unOpenedClose);
+};
 
 module.exports = matchPairs;
--- spec/matchPairsSpec.js
+++ spec/matchPairsSpec.js
@@ -1,61 +1,58 @@
-const matchPairs = require("../matchPairs");
+const matchPairs = require('../matchPairs');
 
-describe("Ensure function throws error with bad inputs", () => {
-    
-    it("throws an error if input not provided", () => {
-        expect(() => matchPairs()).toThrowError();
-    });
+describe('Ensure function throws error with bad inputs', () => {
+  it('throws an error if input not provided', () => {
+    expect(() => matchPairs()).toThrowError();
+  });
 
-    it("throws an error if the input is empty", () => {
-        expect(() => matchPairs("")).toThrowError();
-    });
+  it('throws an error if the input is empty', () => {
+    expect(() => matchPairs('')).toThrowError();
+  });
 
-    it("throws an error if the input isn't a string", () => {
-        expect(() => matchPairs(123)).toThrowError();
-        expect(() => matchPairs(["abc"])).toThrowError();
-    });
+  it("throws an error if the input isn't a string", () => {
+    expect(() => matchPairs(123)).toThrowError();
+    expect(() => matchPairs(['abc'])).toThrowError();
+  });
 
-    it("throws an error if the input string contains invalid char", () => {
-        expect(() => matchPairs("{[3")).toThrowError();
-    });
+  it('throws an error if the input string contains invalid char', () => {
+    expect(() => matchPairs('{[3')).toThrowError();
+  });
 });
 
-describe("Ensure function returns true on good inputs", () => {
+describe('Ensure function returns true on good inputs', () => {
+  it('returns true for braces', () => {
+    expect(matchPairs('{}')).toBe(true);
+  });
 
-    it("returns true for braces", () => {
-        expect(matchPairs("{}")).toBe(true);
-    });
-    
-    it("returns true for brackets", () => {
-        expect(matchPairs("[]")).toBe(true);
-    });
-    
-    it("returns true for parens", () => {
-        expect(matchPairs("()")).toBe(true);
-    });
+  it('returns true for brackets', () => {
+    expect(matchPairs('[]')).toBe(true);
+  });
 
-    it("returns true for nested inputs", () => {
-        expect(matchPairs("{([])}")).toBe(true);
-    });
-    
-    it("returns true for multiple open / close inputs", () => {
-        expect(matchPairs("{}()[]")).toBe(true);
-    });
+  it('returns true for parens', () => {
+    expect(matchPairs('()')).toBe(true);
+  });
+
+  it('returns true for nested inputs', () => {
+    expect(matchPairs('{([])}')).toBe(true);
+  });
+
+  it('returns true for multiple open / close inputs', () => {
+    expect(matchPairs('{}()[]')).toBe(true);
+  });
 });
 
-describe("Ensure function returns false on valid, bad inputs", () => {
+describe('Ensure function returns false on valid, bad inputs', () => {
+  it('returns false if something is not closed', () => {
+    expect(matchPairs('{([])')).toBe(false);
+    expect(matchPairs('{([]}')).toBe(false);
+    expect(matchPairs('{([)}')).toBe(false);
+    expect(matchPairs('[')).toBe(false);
+  });
 
-    it("returns false if something isn't closed", () => {
-        expect(matchPairs("{([])")).toBe(false);
-        expect(matchPairs("{([]}")).toBe(false);
-        expect(matchPairs("{([)}")).toBe(false);
-        expect(matchPairs("[")).toBe(false);
-    });
-
-    it("returns false if something wasn't opened", () => {
-        expect(matchPairs("([])}")).toBe(false);
-        expect(matchPairs("{[])}")).toBe(false);
-        expect(matchPairs("{(])}")).toBe(false);
-        expect(matchPairs("]")).toBe(false);
-    });
+  it('returns false if something was not opened', () => {
+    expect(matchPairs('([])}')).toBe(false);
+    expect(matchPairs('{[])}')).toBe(false);
+    expect(matchPairs('{(])}')).toBe(false);
+    expect(matchPairs(']')).toBe(false);
+  });
 });
